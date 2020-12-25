233321db6ccc26deb59cca9ddb9bf88ba66b2937
9oelM <hj923@hotmail.com>
2020-12-25 18:06:24 +0900

    more cleanups

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,5 @@
 module.exports = {
   someSidebar: {
     Introduction: ['Introduction/0-rationale', 'Introduction/1-ui-is-a-function', 'Introduction/2-pure-functions'],
-    Features: ['mdx'],
   },
 };
